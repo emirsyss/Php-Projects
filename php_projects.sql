e94c2f58c411b799f701f5ceede24c3adfbe299f
emirdev <emirisiklibusiness@gmail.com>
2024-01-13 22:55:49 +0300

    Sql File Commit

--- php_projects.sql
+++ php_projects.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 13 Oca 2024, 01:19:11
+-- Üretim Zamanı: 13 Oca 2024, 20:55:04
 -- Sunucu sürümü: 10.4.28-MariaDB
 -- PHP Sürümü: 8.0.28
 
@@ -20,6 +20,69 @@ SET time_zone = "+00:00";
 --
 -- Veritabanı: `php_projects`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `chat_messages`
+--
+
+CREATE TABLE `chat_messages` (
+  `message_id` int(255) NOT NULL,
+  `sender` varchar(255) NOT NULL,
+  `sender_id` varchar(255) NOT NULL,
+  `message` longtext NOT NULL,
+  `timestamp` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Tablo döküm verisi `chat_messages`
+--
+
+INSERT INTO `chat_messages` (`message_id`, `sender`, `sender_id`, `message`, `timestamp`) VALUES
+(460, 'emirsys', '8', 'SA', '13.01.2024 22:26:18'),
+(461, 'ardasys', '4', 'as', '13.01.2024 22:26:23'),
+(462, 'roselia software', '8', 'as', '13.01.2024 22:27:25'),
+(463, 'roselia software', '8', 'asas', '13.01.2024 22:27:38'),
+(464, 'roselia software', '8', 'sa', '13.01.2024 22:27:38'),
+(465, 'roselia software', '8', 'sa', '13.01.2024 22:27:38'),
+(466, 'roselia software', '8', 'sa', '13.01.2024 22:27:38'),
+(467, 'roselia software', '8', 's', '13.01.2024 22:27:38'),
+(468, 'roselia software', '8', 'as', '13.01.2024 22:27:39'),
+(469, 'roselia software', '8', 'as', '13.01.2024 22:27:39'),
+(470, 'roselia software', '8', 'asas', '13.01.2024 22:27:45'),
+(471, 'ardasys', '4', '<x<zx<zxasf', '13.01.2024 22:28:01'),
+(472, 'ardasys', '4', 'sadasd', '13.01.2024 22:28:03'),
+(473, 'ardasys', '4', 'asd', '13.01.2024 22:28:03'),
+(474, 'ardasys', '4', 'asd', '13.01.2024 22:28:03'),
+(475, 'ardasys', '4', 'asd', '13.01.2024 22:28:03'),
+(476, 'ardasys', '4', 'asd', '13.01.2024 22:28:04'),
+(477, 'ardasys', '4', 'asd', '13.01.2024 22:28:04'),
+(478, 'ardasys', '4', 'a', '13.01.2024 22:28:04'),
+(479, 'ardasys', '4', 'sd', '13.01.2024 22:28:04'),
+(480, 'ardasys', '4', 'asd', '13.01.2024 22:28:04');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `chat_user`
+--
+
+CREATE TABLE `chat_user` (
+  `user_id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `avatar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Tablo döküm verisi `chat_user`
+--
+
+INSERT INTO `chat_user` (`user_id`, `username`, `email`, `password`, `avatar`) VALUES
+(4, 'ardasys', 'ardaisiklibusiness@gmail.com', 'a2d54ea3fcfe904da0f918f9ab77b0adf034e6b9c942e02a7ce1bbcc7afb9fa5', ''),
+(8, 'roselia software', 'emirisiklibusiness@gmail.com', 'a2d54ea3fcfe904da0f918f9ab77b0adf034e6b9c942e02a7ce1bbcc7afb9fa5', '8.png');
 
 -- --------------------------------------------------------
 
@@ -46,6 +109,18 @@ INSERT INTO `mysql_user` (`user_id`, `username`, `password`, `email`) VALUES
 --
 
 --
+-- Tablo için indeksler `chat_messages`
+--
+ALTER TABLE `chat_messages`
+  ADD PRIMARY KEY (`message_id`);
+
+--
+-- Tablo için indeksler `chat_user`
+--
+ALTER TABLE `chat_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- Tablo için indeksler `mysql_user`
 --
 ALTER TABLE `mysql_user`
@@ -54,6 +129,18 @@ ALTER TABLE `mysql_user`
 --
 -- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
 --
+
+--
+-- Tablo için AUTO_INCREMENT değeri `chat_messages`
+--
+ALTER TABLE `chat_messages`
+  MODIFY `message_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=481;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `chat_user`
+--
+ALTER TABLE `chat_user`
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `mysql_user`
